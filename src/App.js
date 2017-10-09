15f9d301285cfddc577eb7efe280f2c785b031fa
Alan Strimbu <astrimbu@gmail.com>
2017-10-09 04:46:07 -0500

    animations, &:hover, fonts, and other goodness

--- src/App.js
+++ src/App.js
@@ -1,39 +1,133 @@
-import React, { Component } from 'react';
-import Dashboard from './Dashboard.js';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { spring, AnimatedSwitch } from 'react-router-transition'
+import ICDTrace from './ICDTrace.js'
 
-class App extends Component {
+
+class DashRow extends Component {
 
   constructor(props) {
     super(props)
-
-    this.handleNavigation = this.handleNavigation.bind(this)
-    this.state = {selectedApp: "Dashboard"}
-  }
-
-  handleNavigation(selection) {
-    console.log('from: handleNavigation(' + selection + ')')
-    this.setState({selectedApp: selection})
   }
 
   render() {
-    let selected = this.state.selectedApp
-    let screen = null
-
-    switch (selected) {
-      case "Dashboard":
-        screen = <Dashboard onNavigation={this.handleNavigation} />
-        break
-      case "ICDTrace":
-        screen = "ICDTrace!"
-        break
-      default:
-        screen = "Other!"
-        break
-    }
-
-    return (<div>{screen}</div>)
+    return (
+      <div className="dash-row">
+          <Link to={this.props.link1[0]} className="dash-item">
+            {this.props.link1[1]}            
+          </Link>
+          <Link to={this.props.link2[0]} className="dash-item">
+            {this.props.link2[1]}
+          </Link>
+      </div>
+    )
   }
 }
 
-export default App;
+class Dashboard extends Component {
 
+  render() {
+    return (
+      <div className="Dashboard">
+        <h2>Dashboard</h2>
+        <DashRow 
+          link1={['/icdtrace','ICDTrace']} 
+          link2={['/ckdrisk','CKD Risk']} />
+        <DashRow
+          link1={['/loinc','LOINC']}
+          link2={['/drugtag','Drug Name Tagging']} />
+        <DashRow
+          link1={['/fraudbilling','Fraudulent Medical Billing']}
+          link2={['/radiology','Radiology']} />
+      </div>
+    )
+  }
+}
+
+const CKDRisk = () => (
+  <div>
+    <h2>CKD Risk</h2>
+  </div>
+)
+
+const LOINC = () => (
+  <div>
+    <h2>LOINC</h2>
+  </div>
+)
+
+const DrugTag = () => (
+  <div>
+    <h2>Drug Name Tagging</h2>
+  </div>
+)
+
+const FraudBilling = () => (
+  <div>
+    <h2>Fraudulent Medical Billing</h2>
+  </div>
+)
+
+const Radiology = () => (
+  <div>
+    <h2>Radiology</h2>
+  </div>
+)
+
+function mapStyles(styles) {
+  return {
+      opacity: styles.opacity,
+      transform: `translateX(${styles.offset}px)`,
+    };
+}
+
+function glide(val) {
+  return spring(val, {
+      stiffness: 127,
+      damping: 17,
+    });
+}
+
+const pageTransitions = {
+  atEnter: {
+      offset: -50,
+      opacity: 0,
+    },
+  atLeave: {
+      offset: glide(-50),
+      opacity: glide(0),
+    },
+  atActive: {
+      offset: glide(0),
+      opacity: glide(1),
+    },
+}
+
+const App = () => (
+  <Router>
+      <div className="App">
+			<div className="Header">
+				<Link to="/">
+          MedTRACE Solutions
+				</Link>
+			</div>
+
+      <hr/>
+
+      <AnimatedSwitch
+          {...pageTransitions}
+          mapStyles={mapStyles}
+          className="switch-wrapper">
+        <Route exact path="/" component={Dashboard}/>
+        <Route path="/icdtrace" component={ICDTrace}/>
+        <Route path="/ckdrisk" component={CKDRisk}/>
+        <Route path="/loinc" component={LOINC}/>
+        <Route path="/drugtag" component={DrugTag}/>
+        <Route path="/fraudbilling" component={FraudBilling}/>
+        <Route path="/radiology" component={Radiology}/>
+      </AnimatedSwitch>
+    </div>
+  </Router>
+)
+
+export default App 
---
+++ src/ICDTrace.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import Box from 'grommet/components/Box'
+import Table from 'grommet/components/Table'
+import TableHeader from 'grommet/components/TableHeader'
+import TableRow from 'grommet/components/TableRow'
+import Button from 'grommet/components/Button'
+import Form from 'grommet/components/Form'
+import TextInput from 'grommet/components/TextInput'
+
+import './grommet-css'
+import './styles/icdtrace.css'
+import registerServiceWorker from './registerServiceWorker'
+
+
+class ICDTrace extends Component {
+
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.selectResult = this.selectResult.bind(this)
+
+    this.state = {
+      diagnosis: '', 
+      results: {"in_text": "", "result": [[]]},
+			request_sent: false,
+			result_obtained: false,
+			rotate: 0,
+      selected: 0
+    }
+  }
+
+  handleChange(event) {
+    this.setState({diagnosis: event.target.value})
+  }
+
+  handleSubmit(event) {
+		const jsonResults = [
+      {"in_text": "pain in left foot", "result": [[0.3077935056255462, 421690, "S99", "Other and unspecified injuries of ankle and foot"], [0.2581988897471611, 382763, "R52", "Pain, unspecified"], [0.2535462764185549, 432978, "T69", "Other effects of reduced temperature"], [0.23717082451262844, 381920, "Q70", "Syndactyly"], [0.23354968324845682, 421461, "S97", "Crushing injury of ankle and foot"], [0.2070196678027063, 421528, "S98", "Traumatic amputation of ankle and foot"], [0.2070196678027063, 364355, "H57", "Other disorders of eye and adnexa"], [0.2032789070454353, 374432, "M79", "Other and unspecified soft tissue disorders, not elsewhere classified"], [0.1999999999999999, 421195, "S96", "Injury of muscle and tendon at ankle and foot level"], [0.19781414201873607, 425052, "T25", "Burn and corrosion of ankle and foot"]]},
+      {"in_text": "right hand", "result": [[0.32732683535398854, 432978, "T69", "Other effects of reduced temperature"], [0.3110855084191276, 371192, "M18", "Osteoarthritis of first carpometacarpal joint"], [0.30618621784789724, 381920, "Q70", "Syndactyly"], [0.280056016805602, 408056, "S69", "Other and unspecified injuries of wrist, hand and finger(s)"], [0.25264557631995577, 371209, "M19", "Other and unspecified osteoarthritis"], [0.24743582965269673, 407673, "S67", "Crushing injury of wrist, hand and fingers"], [0.23570226039551595, 369905, "M07", "Enteropathic arthropathies"], [0.23145502494313774, 407781, "S68", "Traumatic amputation of wrist, hand and fingers"], [0.22176638128637177, 374494, "M80", "Osteoporosis with current pathological fracture"], [0.22176638128637177, 373522, "M63", "Disorders of muscle in diseases classified elsewhere"]]},
+			{"in_text": "inner ear", "result": [[0.2886751345948128, 356534, "C30", "Malignant neoplasm of nasal cavity and middle ear"], [0.26726124191242434, 421804, "T16", "Foreign body in ear"], [0.23570226039551584, 381450, "Q17", "Other congenital malformations of ear"], [0.22941573387056172, 365083, "H82", "Vertiginous syndromes in diseases classified elsewhere"], [0.22941573387056172, 364735, "H67", "Otitis media in diseases classified elsewhere"], [0.19611613513818402, 364778, "H69", "Other and unspecified disorders of Eustachian tube"], [0.19611613513818402, 365166, "H92", "Otalgia and effusion of ear"], [0.18898223650461357, 365243, "H94", "Other disorders of ear in diseases classified elsewhere"], [0.18898223650461357, 364874, "H72", "Perforation of tympanic membrane"], [0.18898223650461357, 364622, "H62", "Disorders of external ear in diseases classified elsewhere"]]}
+		]
+
+    let diagnosis = encodeURIComponent(this.state.diagnosis.trim())
+    let session_url = 'https://acre.cdm.depaul.edu/default/api/v1/instant/ml/50/8?text=' + diagnosis 
+
+    // if request_sent && !result_obtained, show "loading" icon
+    this.setState({result_obtained: false})
+
+    let xhr = new XMLHttpRequest()
+    xhr.onerror = () => {
+			this.setState({result_obtained: true})
+      this.setState({results: jsonResults[this.state.rotate]})
+      if (this.state.rotate < 2) {
+        this.setState({rotate: this.state.rotate + 1})
+      } else {
+        this.setState({rotate: 0})
+      }
+    }
+		xhr.onreadystatechange = () => {
+			switch (this.readyState) {
+				case 0: console.log("unsent")
+          break
+				case 1: console.log("opened")
+          break
+				case 2: console.log("headers_received")
+          break
+				case 3: console.log("loading")
+          break
+				case 4: console.log("done")
+          break
+				case undefined:
+          // xhr issues --> undefined readystate simulates "sent" req
+					this.setState({request_sent: true})
+          break
+				default: break
+			}
+		}
+    xhr.open("POST", session_url, true)
+    const data = window.btoa(unescape(encodeURIComponent('user:pass')))
+    xhr.setRequestHeader("Authorization", "Basic " + data)
+    xhr.send()
+
+    event.preventDefault()
+  }
+
+  selectResult(event) {
+    var t = event.currentTarget
+    this.setState({selected: t.id})
+    // setTimeout because setState calls are batched for performance 
+    // should really use componentDidUpdate
+    setTimeout(() => {
+      this.trimResults(this.state.selected)
+    }, 1);
+  }
+
+  trimResults(selected) {
+    let results = this.state.results
+    console.log(results)
+    results.result.forEach((r) => {
+      if (r[1] == selected) {
+        this.setState({results: {result: [r]}})
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Box align="center" direction="column">
+        <h2>
+          ICDTrace
+        </h2>
+        <Box margin={{vertical: "small"}}>
+          <Form onSubmit={this.handleSubmit} className="diagnosis-form">
+            <h2>
+              Enter Diagnosis:
+            </h2>
+            <TextInput className="diagnosis-text" placeHolder="Diagnosis" onDOMChange={this.handleChange} />
+            <br />
+            <Button type="submit" primary={true} label="Get ICD Codes"
+      className="diagnosis-button" />
+            <br />
+            {this.state.request_sent && !this.state.result_obtained &&
+              <i className="fa fa-spinner fa-pulse fa-fw loading"></i>
+            }
+          </Form>
+        </Box>
+				{this.state.result_obtained &&
+					<Box className="results" 
+            margin={{vertical: "medium"}} 
+            size={{width: {max: "large"}}}>
+						<Table>
+							<TableHeader labels={['Result', 'Code', 'Description']} />
+							<tbody>
+								{this.state.results.result.map((result, i) => {
+                  if (this.state.selected == result[1]) {
+                    return (
+                      <Result conf=" _conf" selectResult={this.selectResult} 
+                      r={result} index={i} />
+                    )
+                  } else {
+                    return (
+                      <Result selectResult={this.selectResult} 
+                      r={result} index={i} />
+                    )
+                  }
+								})}
+							</tbody>
+						</Table>
+					</Box>
+				}
+      </Box>
+    )
+  }
+}
+
+
+class Result extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selected: 0
+    }
+  }
+
+  render() {
+    return (
+      <TableRow className={"results"+this.props.conf} onClick={this.props.selectResult}
+      id={this.props.r[1]} key={this.props.r[1]}>
+        <td>{this.props.index + 1}</td>
+        <td>{this.props.r[2]}</td>
+        <td>{this.props.r[3]}</td>
+      </TableRow>
+    );
+  }
+}
+
+export default ICDTrace
